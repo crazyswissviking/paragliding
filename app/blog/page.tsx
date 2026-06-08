822367167ba2da4e18ea6428bd57a688086ea873
CrazyViking <christianroelli@gmx.ch>
2026-06-08 10:47:05 +0200

    Fix Blog Imports

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 import { useState, useEffect } from "react";
-import { supabase } from "../../supabase";
-import PasswortSchutz from "../passwort";
+import { supabase } from "../supabase";
+import ReactMarkdown from "react-markdown";
 
 type Blog = {
   id: number;
