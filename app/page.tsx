9e21db1214c2c10590c49de5f09c1c8fa0db14ac
VikingFly <christianroelli@gmx.ch>
2026-08-04 19:34:03 +0200

    Alle Events auf Hauptseite 2 Spalten

--- app/page.tsx
+++ app/page.tsx
@@ -85,7 +85,6 @@ export default function Home() {
       const sortiert = (termineData || [])
         .filter((t) => parseDate(t.datum) >= heute.getTime())
         .sort((a, b) => parseDate(a.datum) - parseDate(b.datum))
-        .slice(0, 4);
       setHighlights(sortiert);
 
       const { data: anmeldungenData } = await supabase
